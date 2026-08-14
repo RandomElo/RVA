7b3424ce1a684a9e1a34b6667043af25683fcc3a
Eloi Random <eloi.random@gmail.com>
2026-08-14 21:45:24 +0200

    Suppression du test cron

--- cron/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "🚀 Exécution initiale du script de rapport..."
-node /app/scripts/rapportStatistique.js || echo "⚠️ Échec lors de l'exécution initiale"
-
-echo "⏰ Démarrage du service Cron..."
-exec crond -f -l 2
