415f1a9d668ee99521ec37c7ede3fd65df3a526c
Eloi Random <eloi.random@gmail.com>
2026-09-09 10:52:48 +0200

    Correction pre render n°6 - nettoyage des balises ssr du head

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,5 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 
+document.querySelectorAll('head [data-rh="true"]').forEach((el) => el.remove());
 createRoot(document.getElementById("root")!).render(<App />);
